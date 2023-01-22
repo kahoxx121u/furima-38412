d5026621ddcc70440e2691fa5a2bbfb35fe1e307
kahoxx121u <smileyxoxokaho1212@gmail.com>
2023-01-22 19:23:26 +0900

    商品情報編集機能修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,11 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end 
 
   def edit
-    @item = Item.find(params[:id])
     if current_user.id == @item.user_id 
     else
     redirect_to root_path
@@ -31,7 +30,6 @@ class ItemsController < ApplicationController
   end
 
   def update 
-      @item = Item.find(params[:id])
       if @item.update(item_params)
       redirect_to item_path(item_params)
       else
@@ -50,7 +48,7 @@ class ItemsController < ApplicationController
   end
 
   def set_item
-    #@item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
 end
