c99456fa4b9c21487d38f45a70b8a6711941d86f
kahoxx121u <smileyxoxokaho1212@gmail.com>
2023-02-03 09:07:39 +0900

    エラーハンドリングの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   # ジャンルの選択が「--(id:1)」の時は保存不可
   # 空では登録できない
   validates :item_name,      presence: true
-  validates :text,      presence: true
+  validates :text,      presence: true, allow_blank: true
 
   validates :price,  numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 
